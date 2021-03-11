14a596d84875ddbe0c6075d5f3d7ffeae9766cad
paul-cruz <poljet12@gmail.com>
2021-03-11 12:57:02 -0600

    Calculator UI Added

---
+++ Java/Calculadora/app/src/main/java/mx/ipn/java/calculadora/MainActivity.java
@@ -0,0 +1,18 @@
+package mx.ipn.java.calculadora;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
